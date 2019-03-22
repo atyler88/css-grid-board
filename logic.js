b933e2009831137eb3524908ddfe8bb14aa00c47
atyler88 <a.tyler88@gmail.com>
2019-03-21 20:49:41 -0700

    started a new index.html and logic file

--- logic.js
+++ logic.js
@@ -1,16 +1,49 @@
 const board = {
     tile1: {
+        name: "tile 1",
         resource: 'null',
         number: 0,
         playerOwns: 'null',
         armyNumber: 0,
-    }
-    house1:{
-        neighborTiles: tile1,
-        
-    }
+    },
+    tile2: {
+        name: "Tile 2",
+        resource: 'null',
+        number: 0,
+        playerOwns: 'null',
+        armyNumber: 0,
+    },
+    house1: {
+        name: "House 1",
+        playerOwns: 'null',
+    },
+    house2: {
+        name: "House 2",
+        playerOwns: 'null',
+    },
+    house3: {
+        name: "House 3",
+        playerOwns: 'null',
+    },
 
 }
+tileNieghbors = {
+    tile1: [board.house1, board.house2],
+    tile2: [board.house2, board.house3]
+}
+const playerOne = {
+    name: "player one",
+    bricks: 0,
+    wood: 0,  
+}
+
+const playerTwo = {
+    name: "player one",
+    bricks: 0,
+    wood: 0,
+    
+}
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -21,60 +54,24 @@ let gameHasStarted = false;
 let playerTurn = "null";
 function start() {
 
-    let resources = ["linear-gradient(240deg,rgb(252, 166, 7), rgb(255, 165, 0))","linear-gradient(240deg,rgb(252, 166, 7), rgb(255, 165, 0))","linear-gradient(240deg,rgb(252, 166, 7), rgb(255, 165, 0))","linear-gradient(240deg,rgb(252, 166, 7), rgb(255, 165, 0))", "yellow","yellow","yellow","yellow", "gray","gray","gray","gray", "linear-gradient(240deg, #eaee44, #90ec19)","linear-gradient(240deg, #eaee44, #90ec19)","linear-gradient(240deg, #eaee44, #90ec19)","linear-gradient(240deg, #eaee44, #90ec19)", "linear-gradient(240deg, #f8372a, #cc2900)","linear-gradient(240deg, #f8372a, #cc2900)","linear-gradient(240deg, #f8372a, #cc2900)","linear-gradient(240deg, #f8372a, #cc2900)", "rgb(194, 253, 253", "rgb(194, 253, 253", "rgb(194, 253, 253"];
-    for (let i = 1; i < 24; i++) { 
+    let resources = ["brick", "wood"];
+    for (let i = 1; i < 3; i++) { 
 
         let resource = resources.splice(Math.floor(Math.random()*resources.length), 1);
+        resource = resource.toString();
         tile = "tile" + i
         console.log(tile);
-        document.getElementById(tile).innerHTML =  getRandomInt(2, 13);
-        document.getElementById(tile).style.background = resource;
-        document.getElementById('tile12').removeAttribute("onclick");
+        console.log(resource);
+        board[tile].resource = resource;
     }
     gameHasStarted = true;
-    playerTurn = "red";
 
 }
+
 function roll() {
-    document.getElementById("dice1").innerHTML =  getRandomInt(1, 7);
-    document.getElementById("dice2").innerHTML =  getRandomInt(1, 7);
-}
-function placeHouse(id) {
-    document.getElementById(id).style.background = playerTurn;
-    document.getElementById(id).style.opacity = 1;
-    console.log(id);
-}
-function placeRoad(id) {
-    document.getElementById(id).style.background = playerTurn;
-    document.getElementById(id).style.opacity = 1;
-    console.log(id);
-}
-
-
-function addArmy(id) {
-   
-}
-
-function endTurn() {
-    switch (playerTurn) {
-        case 'red':
-        playerTurn = "blue";
-        console.log('Red players turn has ended');
-        break;
-        case 'blue':
-        playerTurn = "yellow";
-        console.log('Blue players turn has ended');
-        break;
-        case "yellow":
-        playerTurn = "green";
-        console.log('Blue players turn has ended');
-        break;
-        case "green":
-        playerTurn = "red";
-        console.log('Blue players turn has ended');
-        break;
-        default:
-        console.log('Sorry');
-    }
-
+    let roll1 =  getRandomInt(1, 7);
+    let roll2 =  getRandomInt(1, 7);
+    let diceRoll = roll1 + roll2;
+    console.log("the dice are " + roll1 + " and " + roll2)
+    return diceRoll;
 }
