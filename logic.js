ae51be37d131cc95c7712c801a0a213d54ff1b4d
atyler88 <a.tyler88@gmail.com>
2019-03-31 13:33:37 -0700

    finished the house divs and added a hidden class

--- logic.js
+++ logic.js
@@ -7,8 +7,6 @@ const board = {
         playerTwo: 0,
         playerThree: 0,
         playerFour: 0,
-        houseNeighbor: [this.house1, this.house2, this.house3, this.house6, this.house7, this.house8],
-        tileNeighbor: [this.tile2, this.tile3, this.tile4]
     },
     tile2: {
         name: "tile2",
@@ -18,8 +16,6 @@ const board = {
         playerTwo: 0,
         playerThree: 0,
         playerFour: 0,
-        houseNeighbor: [this.house3, this.house4, this.house5, this.house8, this.house9, this.house10],
-        tileNeighbor: [this.tile1, this.tile4, this.tile5]
     },
     tile3: {
         name: "tile3",
@@ -29,8 +25,6 @@ const board = {
         playerTwo: 0,
         playerThree: 0,
         playerFour: 0,
-        houseNeighbor: [this.house6, this.house7, this.house11, this.house12],
-        tileNeighbor: [this.tile1, this.tile4]
     },
     tile4: {
         name: "tile4",
@@ -40,8 +34,6 @@ const board = {
         playerTwo: 0,
         playerThree: 0,
         playerFour: 0,
-        houseNeighbor: [this.house7, this.house8, this.house9, this.house12, this.house13, this.house14],
-        tileNeighbor: [this.tile1, this.tile2, this.tile3, this.tile5]
     },
     tile5: {
         name: "tile5",
@@ -51,8 +43,159 @@ const board = {
         playerTwo: 0,
         playerThree: 0,
         playerFour: 0,
-        houseNeighbor: [this.house9, this.house10, this.house14, this.house15],
-        tileNeighbor: [this.tile2, this.tile4]
+    },
+    tile6: {
+        name: "tile6",
+        resource: null,
+        number: 0,
+        playerOne: 0,
+        playerTwo: 0,
+        playerThree: 0,
+        playerFour: 0,
+    },
+    tile7: {
+        name: "tile7",
+        resource: null,
+        number: 0,
+        playerOne: 0,
+        playerTwo: 0,
+        playerThree: 0,
+        playerFour: 0,
+    },
+    tile8: {
+        name: "tile8",
+        resource: null,
+        number: 0,
+        playerOne: 0,
+        playerTwo: 0,
+        playerThree: 0,
+        playerFour: 0,
+    },
+    tile9: {
+        name: "tile9",
+        resource: null,
+        number: 0,
+        playerOne: 0,
+        playerTwo: 0,
+        playerThree: 0,
+        playerFour: 0,
+    },
+    tile10: {
+        name: "tile10",
+        resource: null,
+        number: 0,
+        playerOne: 0,
+        playerTwo: 0,
+        playerThree: 0,
+        playerFour: 0,
+    },
+    tile11: {
+        name: "tile11",
+        resource: null,
+        number: 0,
+        playerOne: 0,
+        playerTwo: 0,
+        playerThree: 0,
+        playerFour: 0,
+    },
+    tile12: {
+        name: "tile12",
+        resource: null,
+        number: 0,
+        playerOne: 0,
+        playerTwo: 0,
+        playerThree: 0,
+        playerFour: 0,
+    },    
+    tile13: {
+        name: "tile13",
+        resource: null,
+        number: 0,
+        playerOne: 0,
+        playerTwo: 0,
+        playerThree: 0,
+        playerFour: 0,
+    },
+    tile14: {
+        name: "tile14",
+        resource: null,
+        number: 0,
+        playerOne: 0,
+        playerTwo: 0,
+        playerThree: 0,
+        playerFour: 0,
+    },
+    tile15: {
+        name: "tile15",
+        resource: null,
+        number: 0,
+        playerOne: 0,
+        playerTwo: 0,
+        playerThree: 0,
+        playerFour: 0,
+    },
+    tile16: {
+        name: "tile16",
+        resource: null,
+        number: 0,
+        playerOne: 0,
+        playerTwo: 0,
+        playerThree: 0,
+        playerFour: 0,
+    },
+    tile17: {
+        name: "tile17",
+        resource: null,
+        number: 0,
+        playerOne: 0,
+        playerTwo: 0,
+        playerThree: 0,
+        playerFour: 0,
+    },
+    tile18: {
+        name: "tile18",
+        resource: null,
+        number: 0,
+        playerOne: 0,
+        playerTwo: 0,
+        playerThree: 0,
+        playerFour: 0,
+    },
+    tile19: {
+        name: "tile19",
+        resource: null,
+        number: 0,
+        playerOne: 0,
+        playerTwo: 0,
+        playerThree: 0,
+        playerFour: 0,
+    },
+    tile20: {
+        name: "tile20",
+        resource: null,
+        number: 0,
+        playerOne: 0,
+        playerTwo: 0,
+        playerThree: 0,
+        playerFour: 0,
+    },
+    tile21: {
+        name: "tile21",
+        resource: null,
+        number: 0,
+        playerOne: 0,
+        playerTwo: 0,
+        playerThree: 0,
+        playerFour: 0,
+    },
+    tile22: {
+        name: "tile22",
+        resource: null,
+        number: 0,
+        playerOne: 0,
+        playerTwo: 0,
+        playerThree: 0,
+        playerFour: 0,
     },
     house1: {
         type: "empty",
@@ -128,8 +271,242 @@ const board = {
         type: "empty",
         name: "house15",
         playerOwns: null,
-    }
-
+    },
+    house16: {
+        type: "empty",
+        name: "house16",
+        playerOwns: null,
+    },
+    house17: {
+        type: "empty",
+        name: "house17",
+        playerOwns: null,
+    },
+    house18: {
+        type: "empty",
+        name: "house18",
+        playerOwns: null,
+    },
+    house19: {
+        type: "empty",
+        name: "house19",
+        playerOwns: null,
+    },
+    house20: {
+        type: "empty",
+        name: "house20",
+        playerOwns: null,
+    },
+    house21: {
+        type: "empty",
+        name: "house21",
+        playerOwns: null,
+    },
+    house22: {
+        type: "empty",
+        name: "house22",
+        playerOwns: null,
+    },
+    house23: {
+        type: "empty",
+        name: "house23",
+        playerOwns: null,
+    },
+    house24: {
+        type: "empty",
+        name: "house24",
+        playerOwns: null,
+    },
+    house25: {
+        type: "empty",
+        name: "house25",
+        playerOwns: null,
+    },
+    house26: {
+        type: "empty",
+        name: "house26",
+        playerOwns: null,
+    },
+    house27: {
+        type: "empty",
+        name: "house27",
+        playerOwns: null,
+    },
+    house28: {
+        type: "empty",
+        name: "house28",
+        playerOwns: null,
+    },
+    house29: {
+        type: "empty",
+        name: "house29",
+        playerOwns: null,
+    },
+    house30: {
+        type: "empty",
+        name: "house30",
+        playerOwns: null,
+    },
+    house31: {
+        type: "empty",
+        name: "house31",
+        playerOwns: null,
+    },
+    house32: {
+        type: "empty",
+        name: "house32",
+        playerOwns: null,
+    },
+    house33: {
+        type: "empty",
+        name: "house33",
+        playerOwns: null,
+    },
+    house34: {
+        type: "empty",
+        name: "house34",
+        playerOwns: null,
+    },
+    house35: {
+        type: "empty",
+        name: "house35",
+        playerOwns: null,
+    },
+    house36: {
+        type: "empty",
+        name: "house36",
+        playerOwns: null,
+    },
+    house37: {
+        type: "empty",
+        name: "house37",
+        playerOwns: null,
+    },
+    house38: {
+        type: "empty",
+        name: "house38",
+        playerOwns: null,
+    },
+    house39: {
+        type: "empty",
+        name: "house39",
+        playerOwns: null,
+    },
+    house40: {
+        type: "empty",
+        name: "house40",
+        playerOwns: null,
+    },
+    house41: {
+        type: "empty",
+        name: "house41",
+        playerOwns: null,
+    },
+    house42: {
+        type: "empty",
+        name: "house42",
+        playerOwns: null,
+    },
+    house43: {
+        type: "empty",
+        name: "house43",
+        playerOwns: null,
+    },
+    house44: {
+        type: "empty",
+        name: "house44",
+        playerOwns: null,
+    },
+    house45: {
+        type: "empty",
+        name: "house45",
+        playerOwns: null,
+    },
+    house46: {
+        type: "empty",
+        name: "house46",
+        playerOwns: null,
+    },
+    house47: {
+        type: "empty",
+        name: "house47",
+        playerOwns: null,
+    },
+    house48: {
+        type: "empty",
+        name: "house48",
+        playerOwns: null,
+    },
+    house49: {
+        type: "empty",
+        name: "house49",
+        playerOwns: null,
+    },
+    house50: {
+        type: "empty",
+        name: "house50",
+        playerOwns: null,
+    },
+    house51: {
+        type: "empty",
+        name: "house51",
+        playerOwns: null,
+    },
+    house52: {
+        type: "empty",
+        name: "house52",
+        playerOwns: null,
+    },
+    house53: {
+        type: "empty",
+        name: "house53",
+        playerOwns: null,
+    },
+    house54: {
+        type: "empty",
+        name: "house54",
+        playerOwns: null,
+    },
+    house55: {
+        type: "empty",
+        name: "house55",
+        playerOwns: null,
+    },
+    house56: {
+        type: "empty",
+        name: "house56",
+        playerOwns: null,
+    },
+    house57: {
+        type: "empty",
+        name: "house57",
+        playerOwns: null,
+    },
+    house58: {
+        type: "empty",
+        name: "house58",
+        playerOwns: null,
+    },
+    house59: {
+        type: "empty",
+        name: "house59",
+        playerOwns: null,
+    },
+    house60: {
+        type: "empty",
+        name: "house60",
+        playerOwns: null,
+    },
+    house61: {
+        type: "empty",
+        name: "house61",
+        playerOwns: null,
+    },
+    house62: {
+        type: "empty",
+        name: "house62",
+        playerOwns: null,
+    },
 }
 const tileNeighbors = {
     tile1: {
@@ -163,9 +540,9 @@ const houseNeighbors = {
 function start() {
     let start = document.getElementById("start-menu");
     start.classList.add("hide");
-    let resources = ["brick", "wood", "livestock", "wood", "brick"];
-    let numbers = [1, 2, 3, 4, 5];
-    for (let i = 1; i < 6; i++) { 
+    let resources = ["brick", "brick", "brick", "brick", "wood", "wood", "wood", "wood", "livestock", "livestock","livestock","livestock", "ore", "ore", "ore", "ore", "wheat","wheat","wheat","wheat", "water", "water"];
+    let numbers = [3, 4, 5, 6, 7, 8, 9, 10, 11, 3, 4, 5, 6, 7, 8, 9, 10, 11, 5, 6, 8, 10];
+    for (let i = 1; i < 23; i++) { 
 
         let resource = resources.splice(Math.floor(Math.random()*resources.length), 1);
         let number = numbers.splice(Math.floor(Math.random()*resources.length), 1);
@@ -183,12 +560,26 @@ function start() {
         switch(resource) {
             case "brick":
                 element.classList.add("red");
+                element.classList.add("darken-1");
               break;
             case "wood":
                 element.classList.add("green");
+                element.classList.add("darken-2");
               break;
             case "livestock":
-                element.classList.add("yellow");
+                element.classList.add("light-green");
+                element.classList.add("darken-1");
+              break;
+            case "ore":
+                element.classList.add("grey");
+                element.classList.add("darken-1");
+              break;
+            case "wheat":
+                element.classList.add("amber");
+              break;
+            case "water":
+                element.classList.add("light-blue");
+                element.classList.add("accent-1");
               break;
           }
         element.innerHTML = board[tile].number;
@@ -212,6 +603,7 @@ class Player {
             console.log(board[house]);
             let element = document.getElementById(house);
             element.classList.add(this.color);
+            element.classList.remove("hidden");
         }
         let houseHandler = (event) => {
             let e = event.target.id;
